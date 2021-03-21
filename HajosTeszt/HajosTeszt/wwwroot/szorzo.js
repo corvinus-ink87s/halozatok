1893ce66e9a2223c4005a32b75434c4657746fbf
Kovács Hanna <kovacs.hanna.bianka@gmail.com>
2021-03-21 22:40:44 +0100

    szorzo

---
+++ HajosTeszt/HajosTeszt/wwwroot/szorzo.js
@@ -0,0 +1,20 @@
+﻿window.onload = () => {
+
+    let hova = document.getElementById("ide")
+    hova.innerHTML = ""
+
+
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        hova.appendChild(sor)
+        sor.classList.add("egymas_melle")
+
+        for (var o = 0; o < 10; o++) {
+            let szam = document.createElement("div");
+            sor.appendChild(szam)
+            szam.innerText = (s + 1) * (o + 1)
+            szam.classList.add("doboz")
+            szam.style.color = 'rgb(${ 255 / 10 * s},0,${ 255 / 10 * o})'
+        }
+    }
+}
