794cbd2c73c7fe7dfab691306c2efdf453d67398
Kovács Hanna <kovacs.hanna.bianka@gmail.com>
2021-05-16 23:03:44 +0200

    12.gyak - viccek

---
+++ HajosTeszt/HajosTeszt/JokeModels/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.JokeModels
+{
+    public class Joke
+    {
+        public int JokeSk { get; set; }
+        public string JokeText { get; set; }
+        public int UpVotes { get; set; }
+        public int DownVotes { get; set; }
+    }
+}
